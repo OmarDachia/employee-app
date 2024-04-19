721d91187764a3029d6a54f577355193ded5fc6e
Umar Dachia <omardachia@gmail.com>
2024-04-19 14:17:45 +0100

    raw sql query update

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // return view('welcome');
 
-    // $users = DB::select('select * from users');
-    $user = DB::insert('insert into users (name, email, password) values(?,?,?)',['Umar Imam', 'umar.dachias@gmail.com','Password']);
-    dd($user);
-    $users = DB::select('select *from users where email=?',['omardachia@gmail.com']);
-
+    $users = DB::select('select * from users');
+    // $user = DB::insert('insert into users (name, email, password) values(?,?,?)',['Umar Imam', 'umar.dachias@gmail.com','Password']);
+    // dd($user);
+    // $users = DB::select('select *from users where email=?',['omardachia@gmail.com']);
+    $user = DB::update("update users set email = 'umar@gmail.com' where id=2");
     dd($users);
 });
 
